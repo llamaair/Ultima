b4c06d3fb73da0ddc72c8a227856811e5a36d199
llamaair <83296140+llamaair@users.noreply.github.com>
2023-04-27 07:35:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,502 @@ async def ping(ctx):
     latency = (str(client.latency)).split('.')[1][1:3]
     await ctx.respond(f"Pong! Replied in {latency} ms")
 
+#ECONOMY COMMANDS BELOW ONLY
+
+@client.bridge_group()
+async def economy(ctx):
+    pass
+
+async def open_account(user):
+  users = await get_bank_data()
+
+  if str(user.id) in users:
+    return False
+  else:
+    users[str(user.id)] = {}
+    users[str(user.id)]["Wallet"] = 0
+    users[str(user.id)]["Bank"] = 0
+
+  with open("bank.json", 'w') as f:
+    json.dump(users, f)
+
+  return True
+
+async def open_inventory(userinv):
+  usersinv = await get_inventory_data()
+
+  if str(userinv.id) in usersinv:
+    return False
+  else:
+    usersinv[str(userinv.id)] = {}
+    usersinv[str(userinv.id)]["Inventory"] = 0
+
+  with open("inventory.json", "w") as f:
+    json.dump(usersinv, f)
+  
+
+async def get_bank_data():
+  with open("bank.json", 'r') as f:
+    users = json.load(f)
+  
+  return users
+
+async def get_inventory_data():
+  with open("inventory.json", 'r') as f:
+    userinv = json.load(f)
+
+  return userinv
+
+
+
+async def update_bank(user,change = 0,mode = "Wallet"):
+  users = await get_bank_data()
+  users[str(user.id)][mode] += change
+  with open("bank.json","w") as f :
+    json.dump(users,f)
+  bal = [users[str(user.id)]    ["Wallet"],users[str(user.id)]["Bank"]]
+  return bal
+
+async def setmoney(user,change = 0,mode = "wallet"):
+  users = await get_bank_data()
+  users[str(user.id)][mode] = change
+  with open("bank.json","w") as f :
+    json.dump(users,f)
+  bal = [users[str(user.id)]  ["Wallet"],users[str(user.id)]["Bank"]]
+  return bal
+
+@economy.command(description="Set someones balance")
+@commands.has_permissions(administrator=True)
+async def set(ctx,member:discord.Member,amount:int,mode="Wallet"):
+  possible = ["Wallet","Bank"]
+  if mode not in possible : 
+    await ctx.respond(f":x: Where is {mode} ? Please enter bank or wallet.")
+    return
+  await open_account(member)
+  await setmoney(member,amount,mode)
+  await ctx.respond(f":white_check_mark: Set {member.mention}'s {mode} to {amount}")
+  return
+
+@economy.command(description="See your bank balance!")
+async def balance(ctx):
+  await open_account(ctx.author)
+
+  user = ctx.author
+
+  users = await get_bank_data()
+
+  wallet_amt = users[str(user.id)]["Wallet"]
+  bank_amt = users[str(user.id)]["Bank"]
+
+  em = discord.Embed(title=f"{ctx.author.name}'s balance.", color=discord.Color.teal())
+  em.add_field(name="Wallet Balance", value=wallet_amt)
+  em.add_field(name="Bank Balance", value=bank_amt)
+  await ctx.respond(embed=em)
+
+@economy.command(description="See another members' balance")
+async def memberbalance(ctx,member:discord.Member):
+  await open_account(member)
+
+  user = member
+
+  users = await get_bank_data()
+
+  wallet_amt = users[str(user.id)]["Wallet"]
+  bank_amt = users[str(user.id)]["Bank"]
+
+  em = discord.Embed(title=f"{member.name}'s balance.", color=discord.Color.teal())
+  em.add_field(name="Wallet Balance", value=wallet_amt)
+  em.add_field(name="Bank Balance", value=bank_amt)
+  await ctx.respond(embed=em)
+
+@economy.command(description="View your inventory")
+async def inventory(ctx):
+  await open_inventory(ctx.author)
+
+  userinv = ctx.author
+
+  usersinv = await get_inventory_data()
+
+  items = usersinv[str(userinv.id)]["Inventory"]
+
+  if items==1:
+    items="Gun"
+  elif items==2:
+    items="Armour"
+  elif items==3:
+    items="Mouse"
+    
+
+  em = discord.Embed(title=f"{ctx.author.name}s inventory", color=discord.Color.teal())
+  em.add_field(name="Inventory", value=items)
+  await ctx.respond(embed=em)
+
+@economy.command(description="View another members' inventory")
+async def memberinventory(ctx,member:discord.Member):
+  await open_inventory(member)
+
+  userinv = member
+
+  usersinv = await get_inventory_data()
+
+  items = usersinv[str(userinv.id)]["Inventory"]
+
+  if items==1:
+    items="Gun"
+  elif items==2:
+    items="Armour"
+  elif items==3:
+    items="Mouse"
+    
+
+  em = discord.Embed(title=f"{member.name}s inventory", color=discord.Color.teal())
+  em.add_field(name="Inventory", value=items)
+  await ctx.respond(embed=em)
+  
+
+@economy.command(description="Beg for coins!")
+@commands.cooldown(1, 300, commands.BucketType.user)
+async def beg(ctx):
+  await open_account(ctx.author)
+
+  user = ctx.author
+
+  users = await get_bank_data()
+
+  earnings = random.randint(1, 21)
+
+  await ctx.respond(f"Someone gave you {earnings} coins")
+
+  users[str(user.id)]["Wallet"] += earnings
+
+  with open("bank.json", 'w') as f:
+    json.dump(users, f)
+
+@economy.command(description="Go to work")
+@commands.cooldown(1, 300, commands.BucketType.user)
+async def work(ctx):
+  await open_account(ctx.author)
+  user = ctx.author
+  users = await get_bank_data()
+  earnings = random.randint(1, 3)
+  await ctx.respond(f"You earned {earnings} credits for going to work")
+  users[str(user.id)]["Wallet"] += earnings
+  with open("bank.json", 'w') as f:
+    json.dump(users, f)
+    
+@economy.command(description="Get your daily reward!")
+@commands.cooldown(1, 86400, commands.BucketType.user)
+async def daily(ctx):
+  await open_account(ctx.author)
+
+  user = ctx.author
+
+  users = await get_bank_data()
+
+  earnings = random.randint(50, 101)
+
+  await ctx.respond(f"You earned {earnings} from selling some stuff online!")
+
+  users[str(user.id)]["Bank"] += earnings
+
+  with open("bank.json", 'w') as f:
+    json.dump(users, f)
+
+@economy.command(description="Do a bank robbery!")
+@commands.cooldown(1, 2000, commands.BucketType.user)
+async def rob(ctx):
+  await open_account(ctx.author)
+
+  user = ctx.author
+
+  users = await get_bank_data()
+
+  earnings = random.randint(300, 800)
+
+  wallet_amt = users[str(user.id)]["Wallet"]
+
+  decider = random.randint(0,1)
+
+  if decider == 1:
+    await ctx.respond(f"You just robbed the bank and got {earnings}!")
+
+    users[str(user.id)]["Wallet"] += earnings
+
+    with open("bank.json", 'w') as f:
+      json.dump(users, f)
+  else:
+    if wallet_amt > 500:
+      await ctx.respond("The police managed to catch you when you robbed the bank :pensive: They also took 500 credits from you")
+      users[str(user.id)]["Wallet"] -=500
+
+      with open("bank.json", 'w') as f:
+        json.dump(users, f)
+    else:
+      await ctx.respond(f"The police managed to catch you when you robbed the bank, and they also took {wallet_amt} credits from you! :pensive:")
+      users[str(user.id)]["Wallet"] -=wallet_amt
+
+      with open("bank.json", 'w') as f:
+        json.dump(users, f)
+
+@economy.command(description="Give some of your money to another member!")
+async def pay(ctx,amount,member:discord.Member):
+  if amount == None : 
+    return await ctx.respond(":x: Please enter  a proper amount of money!")
+  try :
+    int(amount)
+  except : 
+    return await ctx.respond(":x: Amount can only be a number!")
+  await open_account(ctx.author)
+  await open_account(member)
+  if member == ctx.author :
+    await ctx.respond("It's not a good idea to pay yourself")
+                
+    return
+  bal = await update_bank(ctx.author)
+  if amount == "all":
+    amount = bal[0]
+  try :
+    amount = int(amount)
+  except :
+    await ctx.respond("Please enter a valid number")
+    return
+  if amount>bal[0]:
+    await ctx.respond("Please make sure you have enough money in your wallet!")
+    return
+  if amount<0:
+    await ctx.respond("Please enter a number bigger than 1")
+                
+    return 
+
+  await update_bank(ctx.author,-1*amount,"Wallet")
+  await update_bank(member,amount,"Wallet")  
+
+  await ctx.respond(f":white_check_mark: Transaction completed! {amount} has been transfered to {member.name}")
+    
+
+@economy.command(description="Rob another member!")
+@commands.cooldown(1, 1000, commands.BucketType.user)
+async def robmember(ctx,member:discord.Member):
+  await open_account(ctx.author)
+  await open_account(member)
+  user = ctx.author
+
+  mem = member
+
+  users = await get_bank_data()
+
+  earnings = random.randint(100, 500)
+
+  wallet_aamt = users[str(user.id)]["Wallet"]
+
+  decider = random.randint(0,1)
+
+  wallet_amt = users[str(mem.id)]["Wallet"]
+
+  if wallet_amt < earnings:
+    if wallet_amt < 0:
+      await ctx.respond("It's not worth it :pensive:")
+      return
+    elif wallet_amt == 0:
+      await ctx.respond("It's not worth it :pensive:")
+      return
+    else:
+      earnings = wallet_amt
+
+  
+  if decider == 1:
+    await ctx.respond(f"You just robbed {member} and got {earnings} credits!")
+
+    users[str(user.id)]["Wallet"] +=earnings
+    users[str(mem.id)]["Wallet"] -=earnings
+
+    with open("bank.json", 'w') as f:
+      json.dump(users, f)
+  else:
+    responselist=[f"{mem} knew how to defend themselves and took 100 credits from you instead!", f"{mem} killed you and took 100 credits from you!", "A dog killed you and ate 100 credits!"]
+    choice = random.choice(responselist)
+    await ctx.respond(choice)
+    if wallet_aamt > 100:
+      users[str(user.id)]["Wallet"] -= 100
+
+      with open("bank.json", "w") as f:
+        json.dump(users, f)
+    else:
+      ammountt = wallet_aamt
+      users[str(user.id)]["Wallet"] -=ammountt
+
+      with open("bank.json", 'w') as f:
+        json.dump(users, f)
+      
+    
+
+@economy.command(description="Transfer money from your wallet to the bank")
+async def deposit(ctx,amount):
+  if amount == None : 
+    return await ctx.respond(":x: Please enter a proper amount of money!")
+  try :
+    int(amount)
+  except : 
+    return await ctx.respond(":x: Amount can only be a number!")
+  await open_account(ctx.author)
+          
+  bal = await update_bank(ctx.author)
+  amount = int(amount)
+  if amount>bal[0]:
+    await ctx.respond(":x: You don't have the enough amount !")
+                  
+    return
+  if amount<0:
+    await ctx.respond(":x: Please enter a number bigger than 1.")
+                  
+    return 
+
+  await update_bank(ctx.author,-1*amount)
+  await update_bank(ctx.author,amount,"Bank")  
+
+  await ctx.respond(f":moneybag: You just deposited {amount} dollars.")
+
+@economy.command(description="Transfer money from your wallet to the bank")
+async def withdraw(ctx,amount):
+  if amount == None : 
+    return await ctx.respond(":x: Please enter  a proper amount of money!")
+  try :
+    int(amount)
+  except : 
+    return await ctx.respond(":x: Amount can only be a number!")
+  await open_account(ctx.author)
+  bal = await update_bank(ctx.author)
+  amount = int(amount)
+  if amount>bal[1]:
+    await ctx.respond(":x: You don't have the enough amount !")
+                
+    return
+  if amount<0:
+    await ctx.respond(":x: Please enter a number bigger than 1.")
+                    
+    return 
+        
+  await update_bank(ctx.author,amount)
+  await update_bank(ctx.author,-1*amount,"Bank")  
+
+  await ctx.respond(f":moneybag: You withdrew {amount} dollars.")
+
+@economy.command(description="Buy something")
+async def buy(ctx,item):
+  await open_account(ctx.author)
+  await open_inventory(ctx.author)
+
+  user=ctx.author
+
+  userinv=ctx.author
+
+  cost=2500
+  
+  users=await get_bank_data()
+
+  usersinv=await get_inventory_data()
+
+  wallet_amt = users[str(user.id)]["Wallet"]
+  bank_amt = users[str(user.id)]["Bank"]
+
+  if wallet_amt < cost:
+    if bank_amt > cost:
+      await ctx.respond(f"You don't have enough money in your wallet! Try to buy something online instead with your online wallet! This item costs {cost} credits.")
+      return
+    else:
+      await ctx.respond(f"Buying something costs {cost} currently! You have less than that!")
+      return
+  
+  if item.lower()=="gun":
+    itemcode=1
+    
+    await ctx.respond(f"You just bought {item}! It has been stored in your inventory.")
+
+    users[str(user.id)]["Wallet"] -= cost
+
+    with open("bank.json", "w") as f:
+      json.dump(users, f)
+
+    usersinv[str(userinv.id)]["Inventory"] = itemcode
+
+    with open("inventory.json", "w") as f:
+      json.dump(usersinv, f)
+  elif item.lower()=="armour":
+    itemcode=2
+
+    await ctx.respond(f"You just bought {item}! It has been stored in your inventory.")
+
+    users[str(user.id)]["Wallet"] -= cost
+
+    with open("bank.json", "w") as f:
+      json.dump(users, f)
+
+    usersinv[str(userinv.id)]["Inventory"] = itemcode
+
+    with open("inventory.json", "w") as f:
+      json.dump(usersinv, f)
+
+  elif item.lower()=="mouse":
+    itemcode=3
+
+    await ctx.respond(f"You just bought {item}! It has been stored in your inventory.")
+
+    users[str(user.id)]["Wallet"] -= cost
+
+    with open("bank.json", "w") as f:
+      json.dump(users, f)
+
+    usersinv[str(userinv.id)]["Inventory"] = itemcode
+
+    with open("inventory.json", "w") as f:
+      json.dump(usersinv, f)
+  
+  else:
+    await ctx.respond("This is not a valid item! Use /shop to get a list of purchasable items!")
+
+
+@economy.command(description="View the shop")
+async def shop(ctx):
+  embed=discord.Embed(
+    title="Shop"
+  )
+  embed.add_field(name="Buy things with money!", value="Gun\nArmour\nMouse")
+  await ctx.respond(embed=embed)
+
+@economy.command(description="View the leaderboard")
+async def leaderboard(ctx):
+  limit = 3
+  try :
+            
+    users = await get_bank_data()
+    leader_board = {}
+    total = []
+    for user in users:
+      name = int(user)
+      total_amount = users[user]["Wallet"] + users[user]["Bank"]
+      leader_board[total_amount] = name
+      total.append(total_amount)
+
+    total = sorted(total,reverse=True)    
+
+    em = discord.Embed(title = f"Top {limit} Richest People" , description = "This is decided on the ammount of money in the bank and wallet",color = random.randrange(0, 0xffffff))
+    index = 1
+    for amt in total:
+      id_ = leader_board[amt]
+      member = client.get_user(id_)
+      name = member.name
+      em.add_field(name = f"{index}. {name}" , value = f"{amt}",  inline = False)
+      if index == limit:
+        break
+      else:
+        index += 1
+        em.set_footer(text =f"Requested by {ctx.author}")
+        
+    await ctx.respond(embed = em)
+  except AttributeError:
+    await ctx.respond(":x: Insufficent accounts stored in database!")
+
+
 client.load_extension("cogs.moderation")
 
 client.run(TOKEN)
