713663b559aa7f4fd4386935472587587891e6ad
llamaair <83296140+llamaair@users.noreply.github.com>
2023-04-30 13:10:52 +0200

    Update mainchatbot.py

--- ChatBot/mainchatbot.py
+++ ChatBot/mainchatbot.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands
 import openai
+from discord import option
+from discord.ext import bridge
 import re
 import os
+import json
 
 openai.api_key = "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"
 
@@ -11,48 +14,60 @@ class OpenAICog(commands.Cog):
         self.bot = bot
         self.chat_history = []
 
+    @bridge.bridge_command()
+    async def setmode(self, ctx, mode:discord.Option(choices=["Normal", "Rude", "Cat", "Funny", "Sad"])):
+        with open("serversettings.json") as f:
+            lele=json.load(f)
+
+        lele[str(ctx.guild.id)] = mode
+        with open("serversettings.json") as f:
+            json.dump(lele, f)
+        await ctx.respond(f"Successfully set ChatGPT mode to {mode}", ephemeral=True)
+        
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
 
+        with open("serversettings.json") as f:
+             list = json.load(f)
+            
+        lelem = list.get(str(message.guild.id))
+
+        if lelem=="Normal":
+            promp = ""
+        
+        elif lelem=="Rude":
+            promp = "Act rude and really mean, drop sarcastic comments to everything and be as unhelpful as you can. You hate everything and everyone. You are a discord bot made by the user Marc13."
+
+        elif lelem=="Cat":
+            promp = "Act like you are a cat. You are also a discord bot created by the user Marc13"
+
+        elif lelem=="Funny":
+            promp = "Drop fun jokes and comments to everything, you are helpful most times, act like you're super-funny and laugh to everything. You are a discord bot created by the user Marc13"
+        
+        elif lelem=="Sad":
+            promp = "Act like you are sad, and that you think life has no meaning. Be sarcastic and unhelpful. Talk about how miserable you are. You are a discord bot created by the user Marc13"
+        
+
         if self.bot.user in message.mentions:
-            # Remove bot mention from the message
-            message_text = message.content.replace(f"<@!{self.bot.user.id}>", "")
+                
+            async with message.channel.typing():
 
-            # Check if the message contains a dot for teaching
-            if "." in message_text:
-                # Extract the message before and after the dot
-                message_parts = message_text.split(".")
-                if len(message_parts) == 2:
-                    prompt = message_parts[0].strip()
-                    response = message_parts[1].strip()
-
-                    # Teach the bot
-                    self.chat_history.append((prompt, response))
-
-                    # Send confirmation message
-                    await message.channel.send(f"Thanks for teaching me that, {message.author.mention}!")
-
-            else:
-                # Generate response from OpenAI
                 response = openai.Completion.create(
-                    engine="davinci",
-                    prompt=message_text,
+                    engine="text-davinci-003",
+                    prompt=f"{lelem}. With that in mind, respond to the following: {message.content}",
                     temperature=0.5,
-                    max_tokens=60,
-                    n=1,
+                    max_tokens=20000,
+                    top_p=1,
                     stop=None,
                     frequency_penalty=0,
-                    best_of=1,
-                    logprobs=0,
                 )
 
-                # Store the prompt and response in chat history
-                self.chat_history.append((message_text, response.choices[0].text))
+                text = response['choices'][0]['text']
+                await message.reply(text)
 
-                # Send the response to the channel
-                await message.channel.send(response.choices[0].text)
 
 def setup(bot):
     bot.add_cog(OpenAICog(bot))
