c80259d73ad846df1b82fc53cbc2caa99f82ce42
llamaair <83296140+llamaair@users.noreply.github.com>
2023-04-30 13:57:53 +0200

    Update mainED.py

--- EDBot/mainED.py
+++ EDBot/mainED.py
@@ -6,7 +6,7 @@ class EDDNCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @bridge.bridge_command()
     async def nearest_station(self, ctx, system: str):
         """Finds the nearest station in a given system using EDSM API."""
         response = requests.get(f"https://www.edsm.net/api-system-v1/stations?systemName={system}")
@@ -15,7 +15,7 @@ class EDDNCog(commands.Cog):
             if len(stations) > 0:
                 station_distances = [(station["name"], station["distance_to_star"]) for station in stations]
                 nearest_station = min(station_distances, key=lambda x: x[1])
-                await ctx.send(f"The nearest station in {system} is {nearest_station[0]} ({nearest_station[1]:.2f} ls away).")
+                await ctx.respond(f"The nearest station in {system} is {nearest_station[0]} ({nearest_station[1]:.2f} ls away).")
             else:
                 # No stations in this system, try to find the nearest system with a station
                 response = requests.get(f"https://www.edsm.net/api-system-v1/sphere-systems?x=0&y=0&z=0&radius=50&showInformation=1")
@@ -24,13 +24,13 @@ class EDDNCog(commands.Cog):
                     systems_with_stations = [(system["name"], system["distance"]) for system in systems if system["information"]["stations"] > 0]
                     if len(systems_with_stations) > 0:
                         nearest_system_with_station = min(systems_with_stations, key=lambda x: x[1])
-                        await ctx.send(f"No stations found in {system}. The nearest system with a station is {nearest_system_with_station[0]} ({nearest_system_with_station[1]:.2f} ly away).")
+                        await ctx.respond(f"No stations found in {system}. The nearest system with a station is {nearest_system_with_station[0]} ({nearest_system_with_station[1]:.2f} ly away).")
                     else:
-                        await ctx.send(f"No stations found in {system} or any nearby systems.")
+                        await ctx.respond(f"No stations found in {system} or any nearby systems.")
                 else:
-                    await ctx.send(f"Error getting nearby systems.")
+                    await ctx.respond(f"Error getting nearby systems.")
         else:
-            await ctx.send(f"Error getting stations for {system}.")
+            await ctx.respond(f"Error getting stations for {system}.")
 
 def setup(bot):
     bot.add_cog(EDDNCog(bot))
