a947df74ba5c5285d12bc7dd60e17b4d97acc1c8
llamaair <83296140+llamaair@users.noreply.github.com>
2023-04-30 13:52:03 +0200

    Update mainED.py

--- EDBot/mainED.py
+++ EDBot/mainED.py
@@ -1,32 +1,20 @@
 import discord
 from discord.ext import commands, bridge
 import requests
+import json
 
 class EDDNCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @bridge.bridge_command()
-    async def solsystem(self, ctx):
-        system_name = "Sol"
+    async def sysinfo(self, ctx, system):
 
-        # Prepare EDDN URL and headers
-        url = f"https://eddn.edcd.io/supported-journals?v=1"
-        headers = {"Content-Type": "application/json"}
+        # Prepare EDSM URL and headers
+        url = f"https://www.edsm.net/api-system-v1/bodies?systemName={system}&showId=1"
 
-        # Send request to EDDN to get supported journals
-        response = requests.get(url, headers=headers)
-        supported_journals = response.json()
-
-        # Get the name of the most recent journal from the list of supported journals
-        most_recent_journal_name = supported_journals[-1]["name"]
-
-        # Prepare URL and headers for querying EDDN for system data
-        url = f"https://eddn.edcd.io/supported-journal/{most_recent_journal_name}"
-        headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
-        # Send request to EDDN to get system data for the specified system
-        response = requests.get(url, headers=headers, params={"systemName": system_name})
+        # Send request to EDSM to get system data for the specified system
+        response = requests.get(url)
 
         # Check if the request was successful
         if response.status_code == 200:
@@ -34,10 +22,10 @@ class EDDNCog(commands.Cog):
             # Print the system data to the console
             print(system_data)
             # Send the system data as a message to the Discord channel
-            await ctx.send(f"System data for {system_name}: {system_data}")
+            await ctx.send(f"System data for {system}: {system_data}")
         else:
             # Send an error message to the Discord channel if the request failed
-            await ctx.send("Failed to retrieve system data from EDDN.")
+            await ctx.send("Failed to retrieve system data from EDSM.")
 
 def setup(bot):
     bot.add_cog(EDDNCog(bot))
