52ae3c63fca94c3af6c546ef50698da43153729c
Marc13 <83296140+llamaair@users.noreply.github.com>
2023-05-23 08:35:14 +0200

    fixes

--- EDBot/mainED.py
+++ EDBot/mainED.py
@@ -11,20 +11,27 @@ class EDDNCog(commands.Cog):
         """Finds the nearest station in a given system using EDSM API."""
         response = requests.get(f"https://www.edsm.net/api-system-v1/stations?systemName={system}")
         if response.status_code == 200:
-            stations = response.json()["stations"]
+            data = response.json()
+            stations = data.get("stations", [])  # Use get() method to handle missing key
             if len(stations) > 0:
-                station_distances = [(station["name"], station["distance_to_star"]) for station in stations]
+                station_distances = [(station["name"], station.get("distance_to_star")) for station in stations]
                 nearest_station = min(station_distances, key=lambda x: x[1])
-                await ctx.respond(f"The nearest station in {system} is {nearest_station[0]} ({nearest_station[1]:.2f} ls away).")
+                if nearest_station[1] is not None:
+                    await ctx.respond(f"The nearest station in {system} is {nearest_station[0]} ({nearest_station[1]:.2f} ls away).")
+                else:
+                    await ctx.respond(f"The distance to the star for the nearest station in {system} is not available.")
             else:
                 # No stations in this system, try to find the nearest system with a station
                 response = requests.get(f"https://www.edsm.net/api-system-v1/sphere-systems?x=0&y=0&z=0&radius=50&showInformation=1")
                 if response.status_code == 200:
-                    systems = response.json()["systems"]
-                    systems_with_stations = [(system["name"], system["distance"]) for system in systems if system["information"]["stations"] > 0]
+                    systems = response.json().get("systems", [])  # Use get() method to handle missing key
+                    systems_with_stations = [(system["name"], system.get("distance")) for system in systems if system.get("information", {}).get("stations", 0) > 0]
                     if len(systems_with_stations) > 0:
                         nearest_system_with_station = min(systems_with_stations, key=lambda x: x[1])
-                        await ctx.respond(f"No stations found in {system}. The nearest system with a station is {nearest_system_with_station[0]} ({nearest_system_with_station[1]:.2f} ly away).")
+                        if nearest_system_with_station[1] is not None:
+                            await ctx.respond(f"No stations found in {system}. The nearest system with a station is {nearest_system_with_station[0]} ({nearest_system_with_station[1]:.2f} ly away).")
+                        else:
+                            await ctx.respond(f"The distance to the nearest system with a station from {system} is not available.")
                     else:
                         await ctx.respond(f"No stations found in {system} or any nearby systems.")
                 else:
