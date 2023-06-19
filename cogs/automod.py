25a9a83cd950df8a762b3f300192239ea9cb0b56
Marc13 <marcusolsson123@hotmail.com>
2023-06-19 12:45:33 +0200

    Added debugging for automod

--- cogs/automod.py
+++ cogs/automod.py
@@ -25,8 +25,9 @@ class automod(commands.Cog): # create a class for our cog that inherits from com
             await ctx.guild.create_auto_moderation_rule(name="Anti-spam", reason="Automod", enabled=True, event_type=discord.AutoModEventType.message_send, trigger_type=discord.AutoModTriggerType.spam, trigger_metadata=discord.AutoModTriggerMetadata(), actions=[discord.AutoModAction(discord.AutoModActionType.block_message, metadata)])
             await ctx.guild.create_auto_moderation_rule(name="Words", reason="Automod", enabled=True, event_type=discord.AutoModEventType.message_send, trigger_type=discord.AutoModTriggerType.keyword_preset, trigger_metadata=discord.AutoModTriggerMetadata(presets=discord.AutoModKeywordPresetType["profanity", "sexual_content", "slurs"]), actions=[discord.AutoModAction(discord.AutoModActionType.block_message, metadata)])
             await ctx.respond("AutoMod Enabled! :white_check_mark:")
-        except:
+        except Exception as e:
             await ctx.respond("Failed to enable automod. One reason to this could be that automod is already enabled :x:")
+            print(e)
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(automod(bot)) # add the cog to the bot
