d161c59383eaae5dfe490dc9e5e650274e15a12e
Marc13 <marcusolsson123@hotmail.com>
2023-07-29 19:23:24 +0200

    Update mainapp.py

--- web/mainapp.py
+++ web/mainapp.py
@@ -1,4 +1,4 @@
-from quart import Quart, render_template
+from quart import Quart, render_template_string
 
 class MyAsyncApp(Quart):
     def __init__(self, *args, **kwargs):
@@ -8,7 +8,25 @@ app = MyAsyncApp(__name__)
 
 @app.route('/')
 async def main():
-    return await render_template('dashboard.html')
+
+    template = """
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bot Dashboard</title>
+        <style>
+            /* Add custom CSS styles here */
+        </style>
+    </head>
+    <body>
+        <h1>Bot Dashboard</h1>
+    </body>
+    </html>
+"""
+
+    return await render_template_string(template)
 
 async def startapp():
     await app.run_task(host="0.0.0.0", port=25512)
